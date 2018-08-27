d193bd068c20d9488650b86615812e7fab48ffc9
Kento Dodo <t12552kd@sfc.keio.ac.jp>
2018-08-27 16:43:45 +0900

    add for deploy to raspi

---
+++ command/deploy_develop.sh
@@ -0,0 +1,17 @@
+SCRIPT_DIR=`dirname $0`
+cd $SCRIPT_DIR
+
+BRANCH=develop
+
+cd ../docker
+docker-compose stop
+
+cd ../
+git branch ${BRANCH}
+git checkout ${BRANCH}
+git pull origin ${BRANCH}
+
+cd docker
+docker-compose up -d
+
+echo "Success: deploy on develop"
