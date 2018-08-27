84f1ddf6b9b70d4d84cf7ecfb2ae2131d50fa5f2
Kento Dodo <t12552kd@sfc.keio.ac.jp>
2018-08-27 17:56:02 +0900

    fix function

--- command/deploy.sh
+++ command/deploy.sh
@@ -13,6 +13,8 @@ docker-compose build
 docker-compose run web-kiosk bower install
 docker-compose up -d --force-recreate
 
+sleep 5
+
 export DISPLAY=:0.0 && xdotool key ctrl+F5
 
 echo "Success: deploy on develop"
